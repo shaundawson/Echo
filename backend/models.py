6de559cd5fb48d4bc8f68b31fc5724113ff89565
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2024-04-05 21:09:27 -0400

    Update models.py
    seed data

--- backend/models.py
+++ backend/models.py
@@ -1,15 +1,61 @@
-# models.py
-
 from flask_sqlalchemy import SQLAlchemy
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash
 
 db = SQLAlchemy()
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    # Ensure this is long enough to store a hash
-    password = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    profile = db.relationship('Profile', backref='user', uselist=False)
+    playlists = db.relationship('Playlist', backref='user', lazy=True)
+    posts = db.relationship('Post', backref='user', lazy=True)
+
+
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    bio = db.Column(db.String(255))
+
+
+class Playlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # Add foreign key constraint for the songs relationship
+    songs = db.relationship('Song', backref='playlist', lazy=True)
+
+    # Define foreign key constraint for the songs relationship
+    song_id = db.Column(db.Integer, db.ForeignKey('song.id'), nullable=False)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    song_recommendation = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+class Artist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    albums = db.relationship('Album', backref='artist', lazy=True)
+
+
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    album_name = db.Column(db.String(100), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey(
+        'artist.id'), nullable=False)
+    songs = db.relationship('Song', backref='album', lazy=True)
+
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    artist = db.Column(db.String(100), nullable=False)
+    length = db.Column(db.Integer, nullable=False)
+    album_cover = db.Column(db.String(255))  # Assuming the image path
+    genre = db.Column(db.String(100), nullable=False)
+    album_id = db.Column(db.Integer, db.ForeignKey('album.id'), nullable=False)
