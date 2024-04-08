13cc873864ef365d13390bf838b6fb09dbee0223
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2024-04-08 13:16:22 -0400

    Update app.py
    Add MySQL client library

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-from models import db, User, Profile
+from .models import db, User, Profile
 from dotenv import load_dotenv
 from services import login, get_profile, update_profile, register
 import os
@@ -11,7 +11,8 @@ from flask_migrate import Migrate
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('CLEARDB_DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'CLEARDB_DATABASE_URL').replace('mysql://', 'mysql+pymysql://')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
