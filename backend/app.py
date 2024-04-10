ce79ea4803e52a96986e66583f206d5ad7eeb72f
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2024-04-10 17:08:21 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -21,8 +21,8 @@ database_url = os.environ.get('CLEARDB_DATABASE_URL').replace('mysql://', 'mysql
 
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SESSION_COOKIE_SECURE'] = False # True for prod, False for dev
-app.config['REMEMBER_COOKIE_SECURE'] = False # True for prod, False for dev
+app.config['SESSION_COOKIE_SECURE'] = True # True for prod, False for dev
+app.config['REMEMBER_COOKIE_SECURE'] = True # True for prod, False for dev
 
 # Configure SQLAlchemy engine options
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
@@ -138,8 +138,7 @@ def profile_route(user_id):
         return jsonify({"message": "Method not allowed"}), 405
 
         
-@app.route('/follow/<int:followed_id>', methods=['POST','GET'])
-@cross_origin(supports_credentials=True)
+@app.route('/follow/<int:followed_id>', methods=['POST'])
 def follow_user(followed_id):
     if 'user_id' not in session:
         return jsonify({"message": "Authentication required."}), 401
@@ -161,6 +160,7 @@ def follow_user(followed_id):
     return jsonify({"message": "Now following."}), 200
 
 @app.route('/unfollow/<int:followed_id>', methods=['POST'])
+@cross_origin(supports_credentials=True)
 def unfollow_user(followed_id):
     if 'user_id' not in session:
         return jsonify({"message": "Authentication required."}), 401
