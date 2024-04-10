8bd8a4559fee9ce0d64d7c1d109d959464a5ea49
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2024-04-10 13:54:40 -0400

    fix typo

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-rom flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from backend.models import db, User, Profile
 from dotenv import load_dotenv
