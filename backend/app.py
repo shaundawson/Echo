92db3675d92a335d5e27e610479e5cec9eb906b2
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2024-04-12 19:07:43 -0400

    update dependency versions

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
 import os
+from datetime import timedelta
+
 import redis
-from flask import Flask, request, jsonify, session
+from flask import Flask, request, jsonify, session, redirect, url_for
+from flask_session import Session
 from uuid import uuid4
 from backend.models import db, User, Profile
 from flask_restful import Api, Resource, reqparse
@@ -13,6 +16,7 @@ from flask_cors import CORS, cross_origin
 # Load environment variables
 load_dotenv()
 
+# Create the Flask application
 app = Flask(__name__)
 app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 
@@ -25,6 +29,8 @@ app.config['SESSION_TYPE'] = 'redis'
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_USE_SIGNER'] = True
 app.config['SESSION_REDIS'] = redis_client
+
+server_session = Session(app)
 
 # Configure database
 database_url = os.environ.get('CLEARDB_DATABASE_URL').replace('mysql://', 'mysql+pymysql://')
