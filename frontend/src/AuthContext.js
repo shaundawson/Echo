18ed1a6e4747fbf350c3d2fb578bedd573ec90a5
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2024-04-12 16:18:13 -0400

    Update AuthContext.js

--- frontend/src/AuthContext.js
+++ frontend/src/AuthContext.js
@@ -1,51 +1,35 @@
 import React, { createContext, useContext, useState } from 'react';
-import axios from 'axios';
 
 const AuthContext = createContext();
 
-export const useAuth = () => useContext(AuthContext);
+export function useAuth() {
+    return useContext(AuthContext);
+}
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+    const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem('user')));
+    const [token, setToken] = useState(localStorage.getItem('token'));
 
-
-    const login = async (userData) => {
-        // Make a request to your backend to handle authentication
-        try {
-            const response = await axios.post('https://dry-dawn-86507-cc866b3e1665.herokuapp.com/login', userData, {
-                withCredentials: true // Include credentials for cross-origin requests
-            });
-            setUser(response.data); // Update user state if login is successful
-        } catch (error) {
-            console.error('Login failed:', error);
-            throw new Error('Login failed'); // Handle login failure gracefully
-        }
+    const login = (userData, token) => {
+        setCurrentUser(userData);
+        setToken(token);
+        localStorage.setItem('user', JSON.stringify(userData)); // Persist user data
+        localStorage.setItem('token', token); // Persist token
     };
 
-    // Function to handle logout
-    const logout = async () => {
-        // Make a request to your backend to handle logout
-        try {
-            await axios.post('https://dry-dawn-86507-cc866b3e1665.herokuapp.com/logout', null, {
-                withCredentials: true // Include credentials for cross-origin requests
-            });
-            setUser(null); // Clear user state if logout is successful
-        } catch (error) {
-            console.error('Logout failed:', error);
-            throw new Error('Logout failed'); // Handle logout failure gracefully
-        }
+    const logout = () => {
+        setCurrentUser(null);
+        setToken(null);
+        localStorage.removeItem('user'); // Clear user data
+        localStorage.removeItem('token'); // Clear token
     };
 
-    // Value object to provide to consumers of the AuthContext
     const value = {
-        user,
+        currentUser,
+        token,
         login,
         logout
     };
 
-    return (
-        <AuthContext.Provider value={value}>
-            {children}
-        </AuthContext.Provider>
-    );
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
