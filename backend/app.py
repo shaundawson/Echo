f80ddaeaf1b83b54f91c26d96498cb744d317bb0
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2024-04-14 11:05:51 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,7 @@ app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
 SPOTIFY_REDIRECT_URI = os.environ.get('SPOTIFY_REDIRECT_URI')
+SPOTIFY_REQUIRED_SCOPES = os.environ.get('SPOTIFY_REQUIRED_SCOPES')
 
 # Configure database
 database_url = os.environ.get('CLEARDB_DATABASE_URL').replace('mysql://', 'mysql+pymysql://')
