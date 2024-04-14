676c12a45a7b706541095d73af2d93476e8c2ae7
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2024-04-14 09:57:04 -0400

    Update services.py

--- backend/services.py
+++ backend/services.py
@@ -9,6 +9,7 @@ def generate_key():
     key = Fernet.generate_key()
     with open("secret.key", "wb") as key_file:
         key_file.write(key)
+print(generate_key())
 
 def load_key():
     return open("secret.key", "rb").read()
